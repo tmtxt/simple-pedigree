40d0387d3574010b64d3a8bfe45a60b9c74d9d8d
Hoang Xuan Phu <phunehehe@gmail.com>
2013-04-03 18:26:13 +0700

    default attributes for csync

--- chef/site-cookbooks/main/attributes/default.rb
+++ chef/site-cookbooks/main/attributes/default.rb
@@ -1,6 +1,8 @@
 set[:crowd][:yii_version] = '1.1.13'
 
 default[:crowd][:app_user] = 'crowd'
+default[:crowd][:csync_enable] = false
+default[:crowd][:csync_port] = 0
 default[:crowd][:db][:database] = 'crowd'
 default[:crowd][:db][:host] = 'localhost'
 default[:crowd][:db][:user] = 'crowd'
