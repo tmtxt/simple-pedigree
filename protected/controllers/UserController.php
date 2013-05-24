4d39cb7f45475cd0c2348472e882a7520a71846e
Hoang Xuan Phu <phunehehe@gmail.com>
2013-05-24 14:28:02 +0700

    some more style changes

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -263,12 +263,10 @@ class UserController extends Controller {
                 if ($user->save()) {
                     $this->sendPasswordEmail($user);
                     $this->redirect('/user/resetThanks');
-                }
-                else {
+                } else {
                     MyLog::saveEerror($user, "Could not save new user password");
                 }
-            }
-            else {
+            } else {
                 MyLog::warning("User account not found for user '$reset_user'");
             }
         }
