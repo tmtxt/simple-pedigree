0ea1792364f17e9ff202eb0f06b27d003efe20d2
Hoang Xuan Phu <phunehehe@gmail.com>
2013-07-18 12:21:10 +0700

    mail log route for prod

--- protected/components/MyEmailLogRoute.php
+++ protected/components/MyEmailLogRoute.php
@@ -1,13 +1,8 @@
 <?php
-
 class MyEmailLogRoute extends CEmailLogRoute {
-
     public function getSubject() {
-        $url_length = 40;
         $request_uri = $_SERVER['REQUEST_URI'];
-        if (strlen($request_uri) > ($url_length + 3)) {
-            $request_uri = substr($request_uri, 0, $url_length) . '...';
-        }
-        return parent::getSubject() . ': ' .  $request_uri;
+        return parent::getSubject() . ": " .
+            ((strlen($request_uri) > 43) ? substr($request_uri,0,40) . '...' : $request_uri);
     }
 }
