673b7f216e3e2597bbc1dc139e477c0561110ac5
Long M. K. Nguyen <long@cogini.com>
2014-02-14 15:20:31 +0700

    small fixes

--- chef/site-cookbooks/main/attributes/default.rb
+++ chef/site-cookbooks/main/attributes/default.rb
@@ -11,4 +11,4 @@ default[:skeleton][:python][:virtualenv] = '/home/skeleton/python-env'
 default[:skeleton][:python][:build_dir] = '/home/skeleton/build'
 set[:skeleton][:python][:schemup][:version] = '5f5d35f5c7e9708e62ca43aa4743610e2cb696ae'
 
-default[:skeleton][:environment] = 'dev'
+default[:skeleton][:environment] = 'dev' # 'dev' or 'production'
