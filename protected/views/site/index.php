30924018f971b4a472598abdb11ffd07b8873576
Truong <truong@cogini.com>
2014-08-10 21:42:33 +0700

    delete unnecessary stylesheet files

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,4 +1,9 @@
-<? $this->pageTitle = Yii::app()->name ?>
+<?php
+$clientScript = Yii::app()->clientScript;
+
+$clientScript->registerCssFile('/client/dist/stylesheet/pages/index.css');
+
+$this->pageTitle = Yii::app()->name ?>
 
 <h1><?=Yii::t('app' , 'Welcome' ) ?> <?= CHtml::encode(Yii::app()->name) ?></h1>
 
