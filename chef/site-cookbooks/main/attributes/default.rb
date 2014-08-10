8a7f07ab49143ae45d059ad0933ffe032efe6d36
Truong <truong@cogini.com>
2014-08-10 11:35:26 +0700

    change app user from skeleton to pedigree

--- chef/site-cookbooks/main/attributes/default.rb
+++ chef/site-cookbooks/main/attributes/default.rb
@@ -1,14 +1,14 @@
 set[:skeleton][:yii_version] = '1.1.13'
 
-default[:skeleton][:app_user] = 'skeleton'
+default[:skeleton][:app_user] = 'pedigree'
 default[:skeleton][:csync_enable] = false
 default[:skeleton][:csync_port] = 0
-default[:skeleton][:db][:database] = 'skeleton'
+default[:skeleton][:db][:database] = 'pedigree'
 default[:skeleton][:db][:host] = 'localhost'
-default[:skeleton][:db][:user] = 'skeleton'
+default[:skeleton][:db][:user] = 'pedigree'
 
-default[:skeleton][:python][:virtualenv] = '/home/skeleton/python-env'
-default[:skeleton][:python][:build_dir] = '/home/skeleton/build'
+default[:skeleton][:python][:virtualenv] = '/home/pedigree/python-env'
+default[:skeleton][:python][:build_dir] = '/home/pedigree/build'
 set[:skeleton][:python][:schemup][:version] = '5f5d35f5c7e9708e62ca43aa4743610e2cb696ae'
 
 default[:skeleton][:environment] = 'dev' # 'dev' or 'production'
