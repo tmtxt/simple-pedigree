b5947e7118f65ee58a668f5d6bf90ccf7bf545a6
Truong <truong@cogini.com>
2014-09-08 22:01:30 +0700

    handle disable marriage

--- client/source/javascript/pages/pedigree_tree/node_marriage.js
+++ client/source/javascript/pages/pedigree_tree/node_marriage.js
@@ -28,20 +28,28 @@ function enableMarriage() {
       // append marriage info to the node group
       d3.select(node).append("svg:image")
         .attr("xlink:href", marriage.picture)
+        .attr("class", "marriage-image")
         .attr("x", 0)
         .attr("y", -68)
         .attr("height", "40px")
         .attr("width", "40px")
         .transition()
         .duration(duration)
-        .attr('transform', 'translate (' + 45 * order + 22 + ',0)');
-      order++;
+        .attr('transform', 'translate (' + ((45 * order) + 22) + ',0)');
+      order = order + 1;
     });
   });
 }
 
 function disableMarriage() {
-  
+  var duration = Util.getTransitionDuration();
+
+  // remove all marriage images
+  d3.selectAll('image.marriage-image')
+    .transition()
+    .duration(duration)
+    .attr("transform", "translate(-20,0)")
+    .remove();
 }
 
 function appendMarriage(nodeEnter, enableMarriage) {
