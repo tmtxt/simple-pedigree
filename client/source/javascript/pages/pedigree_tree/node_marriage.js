16e5f7ff95415f2b023b10a73e7c77ccc778541e
Truong <truong@cogini.com>
2014-09-08 22:13:08 +0700

    handle on click for marriage people

--- client/source/javascript/pages/pedigree_tree/node_marriage.js
+++ client/source/javascript/pages/pedigree_tree/node_marriage.js
@@ -25,19 +25,24 @@ function enableMarriage() {
   nodes.forEach(function(node) {
     var order = 0;
     underscore.each(node.__data__.marriages, function(marriage) {
-      // append marriage info to the node group
-      d3.select(node).append("svg:image")
-        .attr("xlink:href", marriage.picture)
-        .attr("class", "marriage-image")
-        .attr("x", 0)
-        .attr("y", -68)
-        .attr("height", "40px")
-        .attr("width", "40px")
-      
-        .transition()
-        .duration(duration)
-        .attr('transform', 'translate (' + ((45 * order) + 22) + ',0)');
-      order = order + 1;
+      if(!!marriage.id) {
+        // append marriage info to the node group
+        d3.select(node).append("svg:image")
+          .attr("xlink:href", marriage.picture)
+          .attr("class", "marriage-image")
+          .attr("x", 0)
+          .attr("y", -68)
+          .attr("height", "40px")
+          .attr("width", "40px")
+          .datum(marriage)
+          .on('click', function(d){
+            Util.showInfoModal(d.id);
+          })
+          .transition()
+          .duration(duration)
+          .attr('transform', 'translate (' + ((45 * order) + 22) + ',0)');
+        order = order + 1;
+      }
     });
   });
 }
@@ -52,18 +57,3 @@ function disableMarriage() {
     .attr("transform", "translate(-20,0)")
     .remove();
 }
-
-function appendMarriage(nodeEnter, enableMarriage) {
-  if(enableMarriage) {
-    nodeEnter.append("svg:image")
-      .attr("xlink:href", function(d){
-        return d.picture;
-      })
-      .attr("x", -20)
-      .attr("y", -68)
-      .attr("height", "40px")
-      .attr("width", "40px")
-    ;
-  }
-}
-exports.appendMarriage = appendMarriage;
