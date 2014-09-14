a00626e156f3ddd7f7560ccf63fc12cb9437fc5a
Truong <truong@cogini.com>
2014-09-14 21:59:09 +0700

    move person detail out of person info modal to a separate module

---
+++ client/source/javascript/views/person_detail.jsx
@@ -0,0 +1,83 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div className="row">
+          
+          <div className="col-md-6">
+            <div className="panel panel-success">
+              <div className="panel-heading">
+                Basic Information
+              </div>
+              <div className="panel-body">
+                <table className="table">
+                  <tr>
+                    <td>Name</td>
+                    <td>{this.props.person.name}</td>
+                  </tr>
+                  <tr>
+                    <td>Birth Date</td>
+                    <td>{this.props.person.birthDate}</td>
+                  </tr>
+                  <tr>
+                    <td>Death Date</td>
+                    <td>{this.props.person.deathDate}</td>
+                  </tr>
+                  <tr>
+                    <td>Gender</td>
+                    <td>{this.props.person.gender}</td>
+                  </tr>
+                  <tr>
+                    <td>Alive Status</td>
+                    <td>{this.props.person.aliveStatus}</td>
+                  </tr>
+                  <tr>
+                    <td>Job</td>
+                    <td>{this.props.person.job}</td>
+                  </tr>
+                  <tr>
+                    <td>Phone No</td>
+                    <td>{this.props.person.phoneNo}</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+          
+          <div className="col-md-6">
+            <div className="panel panel-default">
+              <div className="panel-body">
+                <img className="img-responsive"
+                     src={this.props.person.picture} />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+            <div className="panel panel-success">
+              <div className="panel-heading">History</div>
+              <div className="panel-body">
+                {this.props.person.history}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+            <div className="panel panel-success">
+              <div className="panel-heading">Other Information</div>
+              <div className="panel-body">
+                {this.props.person.otherInformation}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
--- client/source/javascript/views/person_info_modal.jsx
+++ client/source/javascript/views/person_info_modal.jsx
@@ -1,5 +1,7 @@
 var React = require("react");
 
+var PersonDetail = require("./person_detail.jsx");
+
 module.exports = React.createClass({
   render: function() {
     return (
@@ -14,80 +16,7 @@ module.exports = React.createClass({
               </h4>
             </div>
             <div className="modal-body">
-              <div className="row">
-                
-                <div className="col-md-6">
-                  <div className="panel panel-success">
-                    <div className="panel-heading">
-                      Basic Information
-                    </div>
-                    <div className="panel-body">
-                      <table className="table">
-                        <tr>
-                          <td>Name</td>
-                          <td>{this.props.person.name}</td>
-                        </tr>
-                        <tr>
-                          <td>Birth Date</td>
-                          <td>{this.props.person.birthDate}</td>
-                        </tr>
-                        <tr>
-                          <td>Death Date</td>
-                          <td>{this.props.person.deathDate}</td>
-                        </tr>
-                        <tr>
-                          <td>Gender</td>
-                          <td>{this.props.person.gender}</td>
-                        </tr>
-                        <tr>
-                          <td>Alive Status</td>
-                          <td>{this.props.person.aliveStatus}</td>
-                        </tr>
-                        <tr>
-                          <td>Job</td>
-                          <td>{this.props.person.job}</td>
-                        </tr>
-                        <tr>
-                          <td>Phone No</td>
-                          <td>{this.props.person.phoneNo}</td>
-                        </tr>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-                
-                <div className="col-md-6">
-                  <div className="panel panel-default">
-                    <div className="panel-body">
-                      <img className="img-responsive"
-                           src={this.props.person.picture} />
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-md-12">
-                  <div className="panel panel-success">
-                    <div className="panel-heading">History</div>
-                    <div className="panel-body">
-                      {this.props.person.history}
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-md-12">
-                  <div className="panel panel-success">
-                    <div className="panel-heading">Other Information</div>
-                    <div className="panel-body">
-                      {this.props.person.otherInformation}
-                    </div>
-                  </div>
-                </div>
-              </div>
-
+              <PersonDetail person={this.props.person} />
             </div>
             <div className="modal-footer">
               <a href={"/person/addChild/parentId/" + this.props.person.id}
